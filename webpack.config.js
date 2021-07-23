ca60f6ea751ad6ad6f3b8120a4aba6e545cda8a4
Alexander Popov <iiiypuk@iiiypuk.me>
2021-07-23 13:36:37 +0300

    уборка

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './js/index.js',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'app'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'engine.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, 'app'),
+    contentBase: path.join(__dirname, 'src'),
     compress: false,
     port: 55555,
   },
